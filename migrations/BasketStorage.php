e4453f16fc5f6523782aa311e70ba875d0cdeefd
Chris Willerton <lebowskichrisw@gmail.com>
2017-04-11 22:32:54 +0100

    Adds in a migration.

---
+++ migrations/BasketStorage.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BasketStorage extends Migration
+{
+    public function up()
+    {
+        Schema::create('basket_storage', function (Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('key')->index();
+            $table->string('ip_address');
+            $table->text('payload');
+            $table->string('expiry');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('basket_storage');
+    }
+}
