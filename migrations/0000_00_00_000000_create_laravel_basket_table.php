5731dca2c20049b7e3e57157b1352878efea8138
Chris Willerton <lebowskichrisw@gmail.com>
2017-04-13 17:39:18 +0100

    Adds in a working migration.

--- migrations/0000_00_00_000000_create_laravel_basket_table.php
+++ migrations/0000_00_00_000000_create_laravel_basket_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class BasketStorage extends Migration
+class CreateLaravelBasketTable extends Migration
 {
     public function up()
     {
